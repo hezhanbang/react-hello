1c73c615b87e10bc915bd235a4c82b36fc48f998
liyou <liyoubdu@qq.com>
2019-12-27 10:25:46 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
 import HebPlayerPort from './hebPlayerLoader';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.playDiv = React.createRef();
+    this.videoDiv = React.createRef();
   }
 
-  componentDidMount() { 
-    HebPlayerPort.init('http://172.21.4.114:9004/player/hebPlayer.safe.js', true);
-    
-    let fun = function() {
-      HebPlayerPort.playVideo2("34020000001320550001", this.playDiv.current, null);
-    };
-    setTimeout(fun.bind(this), 1000*2);
+  onPlay() {
+    HebPlayerPort.playVideo2("34020000001320550001", this.videoDiv.current, null);
+  }
+
+  onStop() {
+    HebPlayerPort.stopVideo("34020000001320550001");
   }
 
   render() {
     return (
       <div className="App-root">
-        <input className="App-gbID" type="text" value="3402000000132055000x" /><br/>
-        <button type="button">play</button>
-        <button type="button">stop</button>
-        <div ref={this.playDiv} className="App-video" test2="123abcd" ></div>
+        <input className="App-gbID" type="text" defaultValue="3402000000132055000x" /><br/>
+        <button ref={this.play} type="button" onClick={this.onPlay.bind(this)} >play</button>
+        <button ref={this.stop} type="button" onClick={this.onStop.bind(this)} >stop</button>
+        <div ref={this.videoDiv} className="App-video" test2="123abcd" ></div>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import HebPlayerPort from './hebPlayerLoader';
 import './index.css';
+
+HebPlayerPort.init('http://172.21.4.114:9004/player/hebPlayer.safe.js', true);
 
 ReactDOM.render(
   <div><App /><App /><App /></div>,
